6d865471e8b219332dc57e22d01bef464e428d40
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-21 19:57:15 +0800

    Add authenticated base controller

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * application/core/MY_Controller.php | 2026-09-21
+ * Base controller for authenticated application routes.
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller
+{
+    protected $current_user = array();
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (!$this->session->userdata('logged_in'))
+        {
+            $this->session->set_flashdata('flash', array(
+                'type' => 'warning',
+                'message' => 'Please sign in to continue.'
+            ));
+
+            redirect('login');
+            exit;
+        }
+
+        $this->current_user = array(
+            'id' => (int) $this->session->userdata('user_id'),
+            'firstname' => (string) $this->session->userdata('user_firstname'),
+            'lastname' => (string) $this->session->userdata('user_lastname'),
+            'email' => (string) $this->session->userdata('user_email')
+        );
+    }
+
+    protected function require_post()
+    {
+        if (strtoupper($this->input->method()) !== 'POST')
+        {
+            show_error('Method Not Allowed', 405);
+        }
+    }
+
+    protected function set_flash($type, $message)
+    {
+        $this->session->set_flashdata('flash', array(
+            'type' => $type,
+            'message' => $message
+        ));
+    }
+}
