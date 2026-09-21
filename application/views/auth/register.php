1934cda118a5611cf6331e932ed720561cd022d0
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-22 05:19:33 +0800

    Add Bootstrap subresource integrity

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -17,7 +17,7 @@ $old_input = isset($old_input) && is_array($old_input) ? $old_input : array();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register | Curib Employee</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="<?= html_escape(base_url('assets/css/app.css')); ?>" rel="stylesheet">
 </head>
 <body>
@@ -164,7 +164,7 @@ $old_input = isset($old_input) && is_array($old_input) ? $old_input : array();
 
     <?php $this->load->view('components/modals/alert.php'); ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="<?= html_escape(base_url('assets/js/app.js')); ?>"></script>
 </body>
 </html>
