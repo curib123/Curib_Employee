121bc8073d31f6946198b484812e488f9f78894d
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-21 12:27:43 +0800

    Add application/config/autoload.php

---
+++ application/config/autoload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * application/config/autoload.php | 2026-09-21
+ * Autoload configuration for common Employee CRUD dependencies.
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+$autoload['libraries'] = array('database', 'session');
+$autoload['drivers'] = array();
+$autoload['helper'] = array('url', 'form', 'security');
+$autoload['config'] = array();
+$autoload['language'] = array();
+$autoload['model'] = array();
