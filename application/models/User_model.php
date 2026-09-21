c4f03f61c7265999462674a62669ee9d07cf4f33
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-21 19:58:02 +0800

    Use users table for authentication

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends CI_Model
 {
-    private $table = 'user';
+    private $table = 'users';
 
     public function find_by_email($email)
     {
