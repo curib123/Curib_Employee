08e969af1388221bc8e924eae3cc0ecb4d51e30a
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-21 19:56:43 +0800

    Add authentication and dashboard routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,15 +1,24 @@
 <?php
 /**
  * application/config/routes.php | 2026-09-21
- * Routes for the Employee CRUD module.
+ * Authentication, dashboard, and Employee CRUD routes.
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'employees';
+$route['default_controller'] = 'auth/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['employees'] = 'employees/index';
+$route['login']['get'] = 'auth/login';
+$route['login']['post'] = 'auth/login_submit';
+$route['register']['get'] = 'auth/register';
+$route['register']['post'] = 'auth/register_submit';
+$route['registration-password']['get'] = 'auth/registration_password';
+$route['logout']['post'] = 'auth/logout';
+
+$route['dashboard']['get'] = 'dashboard/index';
+
+$route['employees']['get'] = 'employees/index';
 $route['employees/store']['post'] = 'employees/store';
 $route['employees/update/(:num)']['post'] = 'employees/update/$1';
 $route['employees/delete/(:num)']['post'] = 'employees/delete/$1';
