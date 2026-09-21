3f73c82de2dd14d27ec58c75b1f5152ba08757a2
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-22 06:19:53 +0800

    Refine Bootstrap modal UX

--- application/views/components/modals/logout.php
+++ application/views/components/modals/logout.php
@@ -13,18 +13,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     aria-hidden="true"
 >
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content border-0 shadow-lg">
+        <div class="modal-content border-0 shadow-lg rounded-4 overflow-hidden">
             <?= form_open('logout'); ?>
-                <div class="modal-header">
+                <div class="modal-header border-0 bg-body-tertiary px-4 py-3">
                     <h2 class="modal-title fs-5" id="logoutModalLabel">Sign out?</h2>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <p class="mb-0">Your current session will end and you will return to the login page.</p>
+                <div class="modal-body p-4">
+                    <p class="text-secondary mb-0">Your current session will end and you will return to the login page.</p>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Stay signed in</button>
-                    <button type="submit" class="btn btn-danger">Logout</button>
+                <div class="modal-footer border-0 bg-body-tertiary px-4 py-3">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Stay signed in</button>
+                    <button type="submit" class="btn btn-danger px-4">Logout</button>
                 </div>
             <?= form_close(); ?>
         </div>
