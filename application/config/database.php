9226de7044a22c0f74ffaf7a85d986bcc6a59c2c
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-21 12:27:48 +0800

    Add application/config/database.php

---
+++ application/config/database.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * application/config/database.php | 2026-09-21
+ * MySQL connection for database "Curib".
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'      => '',
+    'hostname' => getenv('DB_HOST') ?: 'localhost',
+    'username' => getenv('DB_USER') ?: 'root',
+    'password' => getenv('DB_PASS') ?: '',
+    'database' => getenv('DB_NAME') ?: 'Curib',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8mb4',
+    'dbcollat' => 'utf8mb4_unicode_ci',
+    'swap_pre' => '',
+    'encrypt'  => FALSE,
+    'compress' => FALSE,
+    'stricton' => TRUE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
