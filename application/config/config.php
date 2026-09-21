092ba08ebe34665ba37995817106e6323a2f2fcc
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-21 19:56:41 +0800

    Harden sessions and cookie configuration

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * application/config/config.php | 2026-09-21
- * Core application, session, cookie, and CSRF settings.
+ * Core application, secure session, cookie, and CSRF settings.
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -28,6 +28,7 @@ $config['log_date_format'] = 'Y-m-d H:i:s';
 $config['error_views_path'] = '';
 $config['cache_path'] = '';
 $config['cache_query_string'] = FALSE;
+
 $config['encryption_key'] = getenv('CI_ENCRYPTION_KEY') ?: 'curib-employee-change-this-key';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'curib_employee_session';
@@ -35,20 +36,25 @@ $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
+
 $config['cookie_prefix'] = '';
 $config['cookie_domain'] = '';
 $config['cookie_path'] = '/';
-$config['cookie_secure'] = FALSE;
+$config['cookie_secure'] = (bool) getenv('COOKIE_SECURE');
 $config['cookie_httponly'] = TRUE;
+$config['cookie_samesite'] = 'Lax';
+
 $config['standardize_newlines'] = FALSE;
 $config['global_xss_filtering'] = FALSE;
+
 $config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'curib_csrf_token';
 $config['csrf_cookie_name'] = 'curib_csrf_cookie';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
 $config['csrf_exclude_uris'] = array();
+
 $config['compress_output'] = FALSE;
 $config['time_reference'] = 'local';
 $config['rewrite_short_tags'] = FALSE;
