18f79625ff627c2b59a026d84bf033c09c725a06
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-22 05:19:35 +0800

    Add Bootstrap subresource integrity

--- application/views/auth/registration_password.php
+++ application/views/auth/registration_password.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Save Your Password | Curib Employee</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="<?= html_escape(base_url('assets/css/app.css')); ?>" rel="stylesheet">
 </head>
 <body>
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <?php $this->load->view('components/modals/generated_password.php'); ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="<?= html_escape(base_url('assets/js/auth.js')); ?>"></script>
 </body>
 </html>
