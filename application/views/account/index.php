897bb791710e6911c2ad504df217bfb3ad931a97
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-22 20:41:58 +0800

    Align account form client validation

--- application/views/account/index.php
+++ application/views/account/index.php
@@ -24,8 +24,8 @@
                 <div class="row g-3">
                     <div class="col-md-6"><label class="form-label" for="firstname">First name</label><input class="form-control" id="firstname" name="firstname" maxlength="100" value="<?= html_escape($current_user['firstname']); ?>" required></div>
                     <div class="col-md-6"><label class="form-label" for="lastname">Last name</label><input class="form-control" id="lastname" name="lastname" maxlength="100" value="<?= html_escape($current_user['lastname']); ?>" required></div>
-                    <div class="col-md-6"><label class="form-label" for="birthday">Birthday</label><input class="form-control" type="date" id="birthday" name="birthday" value="<?= html_escape($current_user['birthday']); ?>" required></div>
-                    <div class="col-md-6"><label class="form-label" for="contactno">Contact number</label><input class="form-control" id="contactno" name="contactno" maxlength="20" value="<?= html_escape($current_user['contactno']); ?>" required></div>
+                    <div class="col-md-6"><label class="form-label" for="birthday">Birthday</label><input class="form-control" type="date" id="birthday" name="birthday" min="1900-01-01" max="<?= html_escape(date('Y-m-d')); ?>" value="<?= html_escape($current_user['birthday']); ?>" required></div>
+                    <div class="col-md-6"><label class="form-label" for="contactno">Contact number</label><input class="form-control" type="tel" id="contactno" name="contactno" maxlength="20" pattern="[0-9+()\-\s]{7,20}" inputmode="tel" autocomplete="tel" value="<?= html_escape($current_user['contactno']); ?>" required></div>
                     <div class="col-12"><label class="form-label" for="address">Address</label><textarea class="form-control" id="address" name="address" maxlength="255" rows="3" required><?= html_escape($current_user['address']); ?></textarea></div>
                 </div><button class="btn btn-primary mt-4" type="submit">Update account</button><?= form_close(); ?>
             </div></section>
@@ -41,9 +41,9 @@
             <section class="card border-0 shadow-sm rounded-4 page-section-card"><div class="card-body p-4">
                 <div class="form-section-title mb-3"><h2 class="h5 page-section-title mb-1">Change password</h2><p class="page-section-copy small mb-0">Choose a strong password you do not reuse elsewhere.</p></div>
                 <?= form_open('account/password', array('class' => 'needs-validation', 'novalidate' => 'novalidate')); ?>
-                    <label class="form-label" for="current_password">Current password</label><input class="form-control mb-3" type="password" id="current_password" name="current_password" required>
-                    <label class="form-label" for="new_password">New password</label><input class="form-control mb-3" type="password" id="new_password" name="new_password" minlength="12" maxlength="72" required>
-                    <label class="form-label" for="confirm_password">Confirm new password</label><input class="form-control" type="password" id="confirm_password" name="confirm_password" required>
+                    <label class="form-label" for="current_password">Current password</label><input class="form-control mb-3" type="password" id="current_password" name="current_password" maxlength="72" autocomplete="current-password" required>
+                    <label class="form-label" for="new_password">New password</label><input class="form-control mb-3" type="password" id="new_password" name="new_password" minlength="12" maxlength="72" autocomplete="new-password" required>
+                    <label class="form-label" for="confirm_password">Confirm new password</label><input class="form-control" type="password" id="confirm_password" name="confirm_password" minlength="12" maxlength="72" autocomplete="new-password" required>
                     <div class="form-text">Use at least 12 characters with uppercase, lowercase, a number, and a symbol.</div><button class="btn btn-primary mt-3" type="submit">Update password</button>
                 <?= form_close(); ?>
             </div></section>
